7625e4ae5f765e67e4e102063c08eebad5a7bd7b
shweta <168916522+shwetaanegi@users.noreply.github.com>
2025-10-18 20:24:09 +0530

    Create roman-to-integer.py

---
+++ roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev = 0
+
+        for char in reversed(s):
+            value = roman[char]
+            if value < prev:
+                total -= value
+            else:
+                total += value
+            prev = value
+
+        return total
